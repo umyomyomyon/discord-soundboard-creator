f278462f6cd903b00df3ea5280261fbad54d8812
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-05-28 14:32:27 +0900

    regionをカードに

--- components/Regions.tsx
+++ components/Regions.tsx
@@ -3,6 +3,7 @@ import { Region } from "wavesurfer.js/src/plugin/regions"
 
 import { Icons } from "./icons"
 import { Button } from "./ui/button"
+import { Card, CardContent, CardHeader } from "./ui/card"
 
 type RegionsProps = {
   regions: Region[]
@@ -15,25 +16,29 @@ export const Regions: React.FC<RegionsProps> = React.memo(
       return parseFloat(num.toFixed(3))
     }
     return (
-      <ul>
+      <ul className="flex flex-col gap-4">
         {regions.map((region) => (
-          <li key={region.id} className="flex flex-col gap-4">
-            <div>
-              <p>start: {trimNum(region.start)}s</p>
-              <p>end: {trimNum(region.end)}s</p>
-              <p>total: {trimNum(region.end - region.start)}s</p>
-            </div>
-            <div className="flex gap-4">
-              <Button onClick={() => region.play()}>
-                <Icons.play />
-              </Button>
-              <Button onClick={() => region.remove()}>
-                <Icons.trash />
-              </Button>
-              <Button onClick={() => handleDownload(region)}>
-                ダウンロード
-              </Button>
-            </div>
+          <li key={region.id}>
+            <Card>
+              <CardContent className="flex flex-col gap-4">
+                <div className="mt-6">
+                  <p>start: {trimNum(region.start)}s</p>
+                  <p>end: {trimNum(region.end)}s</p>
+                  <p>total: {trimNum(region.end - region.start)}s</p>
+                </div>
+                <div className="flex gap-4">
+                  <Button onClick={() => region.play()}>
+                    <Icons.play />
+                  </Button>
+                  <Button onClick={() => region.remove()}>
+                    <Icons.trash />
+                  </Button>
+                  <Button onClick={() => handleDownload(region)}>
+                    ダウンロード
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
           </li>
         ))}
       </ul>
