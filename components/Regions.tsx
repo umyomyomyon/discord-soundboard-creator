22cae90dad9866b85dee52b66ba6b4bb7fbe03e0
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-05-28 11:48:11 +0900

    ダウンロードできるように

--- components/Regions.tsx
+++ components/Regions.tsx
@@ -5,21 +5,25 @@ import { Button } from "./ui/button"
 
 type RegionsProps = {
   regions: Region[]
+  handleDownload: (region: Region) => Promise<void>
 }
 
-export const Regions: React.FC<RegionsProps> = React.memo(({ regions }) => {
-  return (
-    <ul>
-      {regions.map((region) => (
-        <li key={region.id}>
-          <p>{region.start}</p>
-          <p>{region.end}</p>
-          <p>{region.end - region.start}秒</p>
-          <Button onClick={() => region.play()}>再生</Button>
-          <Button onClick={() => region.remove()}>削除</Button>
-        </li>
-      ))}
-    </ul>
-  )
-})
+export const Regions: React.FC<RegionsProps> = React.memo(
+  ({ regions, handleDownload }) => {
+    return (
+      <ul>
+        {regions.map((region) => (
+          <li key={region.id}>
+            <p>{region.start}</p>
+            <p>{region.end}</p>
+            <p>{region.end - region.start}秒</p>
+            <Button onClick={() => region.play()}>再生</Button>
+            <Button onClick={() => region.remove()}>削除</Button>
+            <Button onClick={() => handleDownload(region)}>ダウンロード</Button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+)
 Regions.displayName = "Regions"
